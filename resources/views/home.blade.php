a8ad66c0229b2fa5b5cee7f2cba509ad96fa49dd
yuya_mac <gula.yuya1994@gmail.com>
2020-08-08 13:22:25 +0900

    ホーム、入退店管理のブレードを作成

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,17 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3">
-            <div class="card" style="position: sticky; top: 60px">
-                <div class="card-header">sidebar</div>
-                <div class="card-body">
-                    サイドバー
-                </div>
-            </div>
-        </div>
-        <div class="col-9">
+        <div class="col-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">入退店状況</div>
 
                 <div class="card-body" style="padding-bottom: 500px">
                     @if (session('status'))
@@ -22,20 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body" style="padding-bottom: 500px">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    入退店状況
                 </div>
             </div>
         </div>
---
+++ resources/views/userManagement.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">入退店管理</div>
+                <div class="card-body" style="padding-bottom: 500px">
+                    <p>入退店管理</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
